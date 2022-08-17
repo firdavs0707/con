b8d5e4e189792811c44fdf7a3e7689d2e93f6751
firdavs0707 <firdavs0.4.2.0.0.7>
2022-08-17 17:09:02 +0500

    add codes

--- script.js
+++ script.js
@@ -8,23 +8,28 @@ let result = document.querySelector('.output');
 elForm.addEventListener("submit", function(evt){
   evt.preventDefault();
 
-  let select = document.createElement('select');
-  let option1 = document.createElement('option');
-  let option2 = document.createElement('option');
-  let option3 = document.createElement('option');
-  let option4 = document.createElement('option');
-  
-  option1.textContent = `Name: ${elInputName.value}`;
-  option2.textContent = `Surname: ${elInputSurname.value}`;
-  option3.textContent = `Age: ${elInputAge.value}`;
-  option4.textContent = `Email: ${elInputEmail.value}`;
-  select.appendChild(option1)
-  select.appendChild(option2)
-  select.appendChild(option3)
-  select.appendChild(option4)
-  result.appendChild(select)
+  if (elInputName.value == '' || elInputSurname.value == '' || elInputAge.value == '' || elInputEmail.value == '') {
+    alert('Toliq toldirilmadi!')
+  } else {
 
-  select.setAttribute('class', 'd-block mx-auto mb-3')
+    let select = document.createElement('select');
+    let option1 = document.createElement('option');
+    let option2 = document.createElement('option');
+    let option3 = document.createElement('option');
+    let option4 = document.createElement('option');
+    
+    option1.textContent = `Name: ${elInputName.value}`;
+    option2.textContent = `Surname: ${elInputSurname.value}`;
+    option3.textContent = `Age: ${elInputAge.value}`;
+    option4.textContent = `Email: ${elInputEmail.value}`;
+    select.appendChild(option1)
+    select.appendChild(option2)
+    select.appendChild(option3)
+    select.appendChild(option4)
+    result.appendChild(select)
+
+    select.setAttribute('class', 'd-block mx-auto mb-3')
+  }
 })
 
 
